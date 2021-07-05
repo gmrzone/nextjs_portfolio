63be27561bff7ca7efb1f4dc82fd7acbc246c3df
afzal saiyed <saiyedafzalgz@gmail.com>
2021-07-05 17:07:51 +0530

    made navbar fully responsive

--- components/shared/header/Navbar.js
+++ components/shared/header/Navbar.js
@@ -1,6 +1,9 @@
 import NavBarItem from './NavbarItem';
 import NavbarLogo from './NavbarLogo';
+import {useState, useRef} from 'react'
 const Navbar = () => {
+    const [mobileNanActive, setMobileNavActive] = useState(false)
+    const mobileNav= useRef()
     const navData = [
         {
             name: "Home",
@@ -23,18 +26,43 @@ const Navbar = () => {
             to: "#Contact"
         }
     ]
+    const transitionMobileNav = () => {
+        mobileNav.current.classList.remove('translate-x-full')
+        mobileNav.current.classList.add('translate-x-0')
+    }
+    const openMobileNav = () => {
+        mobileNav.current.classList.remove("hidden")
+        mobileNav.current.classList.add('flex')
+        setTimeout(transitionMobileNav, 25)
+        
+    }
+    const hideMobileNav = () => {
+        mobileNav.current.classList.remove("flex")
+        mobileNav.current.classList.add('hidden')
+    }
+    const closeMobileNav = () => {
+        mobileNav.current.classList.remove('translate-x-0')
+        mobileNav.current.classList.add('translate-x-full')
+        setTimeout(hideMobileNav, 500)
+    }
     const renderNavItems = navData.map((x, i) => {
         return <NavBarItem item={x} key={i}/>
     })
     return (
         <nav className="container flex justify-between items-center p-2 mx-5">
             <NavbarLogo />
-            <ul className="flex absolute right-0 top-0 h-screen w-full max-w-full flex-col bg-white text-black shadow-mobile-nav sm:max-w-sm desktop-st:static desktop-st:w-auto desktop-st:bg-transparent desktop-st:text-black desktop-st:max-w-auto desktop-st:h-auto desktop-st:shadow-none">
+            <div className="flex flex-col p-4 desktop-st:hidden space-y-1" onClick={openMobileNav}>
+                <span className="bg-gray-800 h-1 w-8"></span>
+                <span className="bg-gray-800 h-1 w-10"></span>
+                <span className="bg-gray-800 h-1 w-10"></span>
+                <span className="bg-gray-800 h-1 w-10"></span>
+            </div>
+            <ul className="hidden fixed right-0 translate-x-full transition-transform duration-500 top-0 h-full w-full max-w-full flex-col bg-white text-black shadow-mobile-nav sm:max-w-sm desktop-st:flex desktop-st:static desktop-st:w-auto desktop-st:bg-transparent desktop-st:text-black desktop-st:max-w-auto desktop-st:h-auto desktop-st:shadow-none desktop-st:max-w-max desktop-st:-translate-x-0" ref={mobileNav}>
                 <div className="flex flex-row align-middle justify-items-start py-4 px-4 text-gray-800 border-b border-gray-200 sm:px-6 desktop-st:hidden">
-                    <i className="far fa-times text-2xl cursor-pointer" />
+                    <i className="far fa-times text-2xl cursor-pointer" onClick={closeMobileNav}/>
                     <div className="text-xl font-semibold w-full text-center leading-8">Close</div>
                 </div>
-                <div className="flex flex-col desktop-st:flex-row text-center mt-10 mb-auto desktop-st:space-x-10 desktop-st:mt-0">   
+                <div className="flex flex-col text-center mt-10 mb-auto desktop-st:flex-row desktop-st:space-x-10 desktop-st:mt-0">   
                     {renderNavItems}
                 </div>
             </ul>
