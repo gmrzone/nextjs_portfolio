d82a34870dfb2b615afd4b36358dbfbf634fb1a0
afzal saiyed <saiyedafzalgz@gmail.com>
2021-07-06 16:22:48 +0530

    Added Icons for Skills

--- components/home/skills/SkillsAccordian.js
+++ components/home/skills/SkillsAccordian.js
@@ -1,18 +1,19 @@
 const SkillsAccordian = ({title, data}) => {
-    // const renderData = data.map((x, i) => {
-    //     return (
-    //         <div key={i} className="">
-    //             <div>{x.name}</div>
-    //             <div>{x.skill}</div>
-    //         </div>
-    //     )
-    // })
+    const renderData = data.map((x, i) => {
+        return (
+            <div key={i} className="">
+                <div>{x.name}</div>
+                <div>{x.skill}</div>
+            </div>
+        )
+    })
     return (
         <div>  
             <div>
                 <h3>{title}</h3>
             </div>
             <div>
+                {renderData}
             </div>
         </div>
     )
--- components/home/skills/index.js
+++ components/home/skills/index.js
@@ -3,94 +3,113 @@ const Skills = () => {
     const frontEndData = [
         {
             name: "HTML",
-            skill: 90
+            skill: 90,
+            icon: "fab fa-html5"
         },
         {
             name: "CSS",
-            skill: 75
+            skill: 75,
+            icon: "fab fa-css3-alt"
         },
         {
             name: "SCSS",
-            skill: 75
+            skill: 75,
+            "icon": "fab fa-sass"
         },
         {
             name: "Tailwind CSS",
-            skill: 75
+            skill: 75,
+            "src": "./tailwind.svg"
         },
         {
             name: "Javascript",
-            skill: 80
+            skill: 80,
+            icon: "fab fa-js"
         },
         {
             name: "React",
-            skill: 90
+            skill: 85,
+            icon: "fab fa-react"
         },
         {
             name: "Redux",
-            skill: 90
+            skill: 80,
+            src: "./redux.svg"
         },
         {
             name: "Next JS",
-            skill: 90
+            skill: 85,
+            "src": "./next-js.svg"
         },
         {   
             name: "Chart.js",
-            skill: 85
+            skill: 85,
+            src: "./chartjs.svg"
         },
 
     ]
     const BackendData = [
         {
             name: "Python",
-            skill: 92
+            skill: 85,
+            icon: "fab fa-python"
         },
         {
             name: "Django / DRF",
-            skill: 90
+            skill: 83,
+            src: "./django.svg"
         },
         {
             name: "PostgreSql",
-            skill: 65
+            skill: 65,
+            src: "./postgresql.svg"
         },
         {
             name: "Redis",
-            skill: 65
+            skill: 65,
+            src: "./redis.svg"
         },
         {
             name: "Node",
-            skill: 35
+            skill: 35,
+            icon: "fab fa-node"
         },
 
     ]
     const OthersData = [
         {
             name: "Git CLI",
-            skill: 75
+            skill: 75,
+            icon: "fab fa-github"
         },
         {
             name: "Vs Code",
-            skill: 75
+            skill: 75,
+            src: "./vscode.svg"
         },
         {
             name: "Docker / Docker Compose",
-            skill: 60
+            skill: 60,
+            icon: "fab fa-docker"
         },
         {
             name: "Linux",
-            skill: 75
+            skill: 75,
+            icon: "fab fa-linux"
         },
         {
             name: "Windows",
-            skill: 95
+            skill: 95,
+            icon: "fab fa-windows"
         }
-        
+
     ]
     return (
         <div className="mt-6">
             <div className="container">
                 <h2 className="text-main">Skills</h2>
                 <div className="mt-6">
-                    <SkillsAccordian title="Backend"/>
+                    <SkillsAccordian title="Backend" data={BackendData}/>
                 </div>
             </div>  
         </div>
