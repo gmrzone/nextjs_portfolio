a74bd4ebff5d999c74ec64f6d52efb1843a1eeb9
afzal saiyed <saiyedafzalgz@gmail.com>
2021-07-07 18:01:24 +0530

    added Intersection Objerver animation on About Section

--- components/home/about/AboutPhoto.js
+++ components/home/about/AboutPhoto.js
@@ -2,7 +2,7 @@ import photo from '../../../public/placeholder.png';
 import Image from 'next/image'
 const AboutPhoto = () => {
     return (
-        <div className="w-full mr-0 mb-6 desktop-st:mr-8 desktop-st:mb-0 desktop-st:w-1/3">   
+        <div className="w-full mr-0 mb-6 desktop-st:mr-8 desktop-st:mb-0 desktop-st:w-1/3 -translate-x-full opacity-0 transition-all duration-500" data-name="photo">   
             <div className="rounded-md overflow-hidden">
                 <Image alt="profile_pic" src={photo} placeholder="blur" width={380} height={380} className="rounded-md"/>
             </div>
--- components/home/about/index.js
+++ components/home/about/index.js
@@ -1,11 +1,43 @@
 import AboutPhoto from './AboutPhoto';
-import AboutContent from './AboutContent'
+import AboutContent from './AboutContent';
+import { useEffect, useRef } from 'react';
 const About = () => {
+    const aboutContentRef = useRef()
+    useEffect(()=> {
+         const options = {
+            rootMargin: '0px 0px -100px 0px',
+         }
+         const observer = new IntersectionObserver((entries, observer)=> {
+              entries.forEach(x => {
+                  if (x.isIntersecting){
+                      console.log(x.target)
+                      console.log(x.target.dataset.name)
+                      if (x.target.dataset.name === "photo"){
+                          x.target.classList.remove('-translate-x-full')
+                          x.target.classList.remove('opacity-0')
+                          x.target.classList.add('translate-x-0')
+                          x.target.classList.add('opacity-100')
+                      }
+                      else if (x.target.dataset.name === "content"){
+                          x.target.classList.remove('translate-x-full')
+                          x.target.classList.remove('opacity-0')
+                          x.target.classList.add('translate-x-0')
+                          x.target.classList.add('opacity-100')
+                      }
+                  }
+
+              })
+         }, options)
+
+         Array.from(aboutContentRef.current.children).forEach(x => {
+            observer.observe(x)
+         })
+    }, [])
     return (
         <div className="mt-6 desktop-st:mt-12">
             <div className="container">
                 <h2 className="text-main text-center">About</h2>
-                <div className="flex flex-col text-center mt-6 desktop-st:mt-10 desktop-st:flex-row desktop-st:text-left">
+                <div className="flex flex-col text-center mt-6 desktop-st:mt-10 desktop-st:flex-row desktop-st:text-left" ref={aboutContentRef}>
                     <AboutPhoto />
                     <AboutContent />
                 </div>
