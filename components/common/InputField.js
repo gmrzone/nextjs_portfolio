6264309331d69c21748ffba1b56231d63108a31c
afzal saiyed <saiyedafzalgz@gmail.com>
2021-07-08 04:33:54 +0530

    added animation to input fields

--- components/common/InputField.js
+++ components/common/InputField.js
@@ -1,11 +1,13 @@
 import style from '../../styles/input.module.scss'
 const InputField = ({name, ...attrs}) => {
     return (
-        <label className={style['custom-input']}>
-            <input {...attrs} className={style['main-input']}/>
-            <span className={style['custom-input__placeholder']}>{name}</span>
-            <span className={style['custom-border']}></span>
-        </label>
+        <div className={style['main-container']}>
+            <label className={style['custom-input']}>
+                <input {...attrs} className={style['main-input']}/>
+                <span className={style['custom-input__placeholder']}>{name}</span>
+                <span className={style['custom-border']}></span>
+            </label>
+        </div>
     )
 }
 
