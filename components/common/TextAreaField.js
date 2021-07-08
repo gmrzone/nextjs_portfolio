c662f946f82013ff8ed324049560ec41c36a0b94
afzal saiyed <saiyedafzalgz@gmail.com>
2021-07-08 17:06:50 +0530

    added custom textarea

---
+++ components/common/TextAreaField.js
@@ -0,0 +1,24 @@
+import style from '../../styles/textarea.module.scss';
+import {useRef} from  "react";
+const TextAreaField = ({name, ...attrs}) => {
+    const inputRef = useRef()
+    const handleBlur = (e) => {
+        if (e.target.value){
+            inputRef.current.classList.add(style['not-empty'])
+        }
+        else{
+            inputRef.current.classList.remove(style['not-empty'])
+        }
+    }
+    return (
+        <div className={style['main-container']}>
+            <label className={style['custom-input']} ref={inputRef}>
+                <textArea {...attrs} className={style['main-input']} onBlur={handleBlur}/>
+                <span className={style['custom-input__placeholder']}>{name}</span>
+                <span className={style['custom-border']}></span>
+            </label>
+    </div>
+    )
+}
+
+export default TextAreaField
--- components/home/contact/contactForm.js
+++ components/home/contact/contactForm.js
@@ -1,8 +1,11 @@
-import InputField from '../../common/InputField'
+import InputField from '../../common/InputField';
+import TextAreaField from '../../common/TextAreaField';
 const ContactForm = () => {
     return (
-        <form>
+        <form className="space-y-2">
             <InputField type="text" name="name"/>
+            <InputField type="email" name="Email"/>
+            <TextAreaField name="message"/>
         </form>
     )
 }  
