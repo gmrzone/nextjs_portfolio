73704f4268f93b70f74f7a465f0cdd803c333faf
afzal saiyed <saiyedafzalgz@gmail.com>
2021-07-08 15:31:07 +0530

    input field completed

--- components/common/InputField.js
+++ components/common/InputField.js
@@ -1,9 +1,19 @@
-import style from '../../styles/input.module.scss'
+import style from '../../styles/input.module.scss';
+import {useRef} from  "react";
 const InputField = ({name, ...attrs}) => {
+    const inputRef = useRef()
+    const handleBlur = (e) => {
+        if (e.target.value){
+            inputRef.current.classList.add(style['not-empty'])
+        }
+        else{
+            inputRef.current.classList.remove(style['not-empty'])
+        }
+    }
     return (
         <div className={style['main-container']}>
-            <label className={style['custom-input']}>
-                <input {...attrs} className={style['main-input']}/>
+            <label className={style['custom-input']} ref={inputRef}>
+                <input {...attrs} className={style['main-input']} onBlur={handleBlur}/>
                 <span className={style['custom-input__placeholder']}>{name}</span>
                 <span className={style['custom-border']}></span>
             </label>
