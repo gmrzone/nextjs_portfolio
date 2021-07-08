0f4ad4b79f9bae84d776e93a5b36b43eef463b36
afzal saiyed <saiyedafzalgz@gmail.com>
2021-07-09 04:05:53 +0530

    remove nodemail

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,17 +1,39 @@
+
+const mail = require('@sendgrid/mail');
+mail.setApiKey(process.env['SEND_GRID_API_KEY'])
+
+
+const contact = (req,res) => {
+    const msg = {
+        to: process.env['TO_EMAIL'],
+        from: process.env['EMAIL'], // Use the email address or domain you verified above
+        subject: `Message by ${req.body.name}`,
+        text: `Name: ${req.body.name}\nEmail: ${req.body.email}\nMessage: ${req.body.message}`,
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      };
+      mail.send(msg)
+      .then(() => {
+        res.status(200).json({status: 'ok', message:`Hello ${req.body.name}, Your message was send sucessfully.`})
+      })
+      .catch(e => {
+        res.status(403).json({status: 'error', message:"There was a problem with the server. Please try again later."})
+      })
+      
+}
+
+export default contact
+
+
+
+// const nodemailer = require('nodemailer');
+// const nodemailerSendgrid = require('nodemailer-sendgrid');
+
 // const contact = async (req, res) => {
-//     let nodemailer = require('nodemailer')
-//     const transporter = nodemailer.createTransport({
-//         port: 465,
-//         host: "smtp.gmail.com",
-//         auth: {
-//         user: process.env['EMAIL'],
-//         pass: process.env['PASSWORD'],
-//         },
-//         tls: {
-//             rejectUnauthorized: false
-//         }
-//         // secure: true,
-//     });
+//     const transport = nodemailer.createTransport(
+//         nodemailerSendgrid({
+//             apiKey: process.env['SEND_GRID_API_KEY']
+//         })
+//     );
 //     const mailData = {
 //         from: process.env['EMAIL'],
 //         to: process.env['TO_EMAIL'],
@@ -20,47 +42,16 @@
         
        
 //        }
-//     transporter.sendMail(mailData, function(err, success){
-//         if (err){
-            
-//             res.status(401).json({status: "error", message: "Something is wrong with the server please try again later"})
-//         }
-//         else{
-//             res.status(200).json({status: 'ok', message:`Hello ${req.body.name}, Your message was send sucessfully.`})
-//         }
-//     })
+//        transport.sendMail(mailData, (error, success) => {
+//             if (error){
+                
+//                 res.status(401).json({status: "error", message: "Something is wrong with the server please try again later"})
+//             }
+//             else{
+//                 res.status(200).json({status: 'ok', message:`Hello ${req.body.name}, Your message was send sucessfully.`})
+//             }
+//         });
     
 // }
 
 // export default contact
-
-const nodemailer = require('nodemailer');
-const nodemailerSendgrid = require('nodemailer-sendgrid');
-
-const contact = async (req, res) => {
-    const transport = nodemailer.createTransport(
-        nodemailerSendgrid({
-            apiKey: process.env['SEND_GRID_API_KEY']
-        })
-    );
-    const mailData = {
-        from: process.env['EMAIL'],
-        to: process.env['TO_EMAIL'],
-        subject: `Message From ${req.body.name}`,
-        text: `Name: ${req.body.name}\nEmail: ${req.body.email}\nMessage: ${req.body.message}`,
-        
-       
-       }
-       transport.sendMail(mailData, (error, success) => {
-            if (error){
-                
-                res.status(401).json({status: "error", message: "Something is wrong with the server please try again later"})
-            }
-            else{
-                res.status(200).json({status: 'ok', message:`Hello ${req.body.name}, Your message was send sucessfully.`})
-            }
-        });
-    
-}
-
-export default contact
