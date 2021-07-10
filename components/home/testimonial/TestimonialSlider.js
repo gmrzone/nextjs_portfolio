856c0460201126c252481e71cccfd0d316b47232
afzal saiyed <saiyedafzalgz@gmail.com>
2021-07-10 19:15:05 +0530

    added comments

--- components/home/testimonial/TestimonialSlider.js
+++ components/home/testimonial/TestimonialSlider.js
@@ -10,11 +10,12 @@ const TestimonialSlider = () => {
             <SliderItems item={x} key={x.id}/>
         )
     })
-    const currentTransPosition = useRef(0);
-    const currentTransPosition1 = useRef(0);
-    const maxRightTransition = useRef(0)
-    let startTouchPosition = null;
-    let currentTouchPosition = null;
+    const currentTransPosition = useRef(0);    // variable to capture current transition value in mouse move or touch move
+    const currentTransPosition1 = useRef(0);   // variavle to capture current transition value in mouse move or touch move
+    const maxRightTransition = useRef(0);      // variable to keep track of maxRightTransition to avoid slider from getting outside of wrapper container
+    let startPointerCapturePosition = null;    // variable to capture start touch or mouse drag position in handlePointerStart callback
+    let currentPointerCapturePosition = null;  // variable to capture current touch position or douse drag position in handlePointerMove callback
+    let mousePressed = false //                // variable to keep track if the mouse button is pressed so that the slider only slides when dragging with mouse not when moving the mouse over it (for Mouse event only)
 
     // reset slider transition position to zero on window resize to avoid layout mess (workaround)
     useEffect(() => {
@@ -27,31 +28,15 @@ const TestimonialSlider = () => {
     }, [])
 
 
-    let mousePressed = false
-
-    const handleMouseUp = (e) => {
-        console.log("Mouse Leave", e)
-        mousePressed = false
-    }
-
-
-    const handleMouseMove = (e) => {
-        if (mousePressed){
-            console.log(e)
-        }
-        
-    }
-
-
-    // this event callback is triggered on touch start. Save touch start position so we can compare it with current touch position to find if the user has swiped left or right
-    const handleTouchStart = (e) => {
+    // this event callback is triggered on touch start and mouse down . Save touch start position of mouse down button press position so we can compare it with current touch position or mouse up button position to find if the user has swiped left or right
+    const handlePointerStart = (e) => {
         if (e.type === "mousedown"){
             e.preventDefault()
-            startTouchPosition = e.clientX
+            startPointerCapturePosition = e.clientX
             mousePressed = true
         }
         else{
-            startTouchPosition = e.touches[0].clientX;
+            startPointerCapturePosition = e.touches[0].clientX;
                                 
 
         }
@@ -59,12 +44,13 @@ const TestimonialSlider = () => {
         
     }
 
-    // this event callback is triggered when the touch end in this callback first we check if the slider is transitioned more then its container on
-    // left and right during touch move event if its transitioned more then 0 on left we reset it to zero and if its transitioned more then container width to right
+    // this event callback is triggered when the touch end of mouse up. in this callback first we check if the slider is transitioned more then its container on
+    // left and right during touch move or mouse move event if its transitioned more then 0 on left we reset it to zero and if its transitioned more then container width to right
     // we reset its transitioned to containers width. This way the slider does not go outside container from both left and right.
-    // next if startTouchPosition and currentTouchPosition is captured by touchStart and touchMove event callback we find out if its a left swipe or right swipe and depending on 
+    // next if startPointerCapturePosition and currentPointerCapturePosition is captured by handlePointerStart and handlePointerMove event callback we find out if its a left swipe or right swipe and depending on 
     // that we increase or decrease the transition of the container by sliderItem width + padding to meke it slide exactly 1 item inside the container
-    const handleTouchEnd = (e) => {
+    const handlePointerEnd = (e) => {
+        // It the event type is mouseup the save the status on mousePressed so we can stop sliding the slider in handlePoinerMove
         if (e.type === "mouseup"){
             mousePressed = false
         }
@@ -87,10 +73,10 @@ const TestimonialSlider = () => {
             currentTransPosition.current = maxRightTransition.current
         }
         
-        if (currentTouchPosition){
+        if (currentPointerCapturePosition){
             let sliderItemWidth = mainContainer.current.firstChild.clientWidth
             
-            if (currentTouchPosition > startTouchPosition){
+            if (currentPointerCapturePosition > startPointerCapturePosition){
                 if (currentTransPosition1.current < 0){
                     const ratio = Math.ceil(currentTransPosition.current / (sliderItemWidth + 20))
                     const transitionTo = ((sliderItemWidth * ratio) + ratio * 20)
@@ -114,23 +100,22 @@ const TestimonialSlider = () => {
         }
     }
     // Touch Move Event. This callback will slide the slider as user holds and move touch
-    // This event callback is triggered when user hold touch and move. In this callback we capture currentTouchposition and if the currentTouchPosition is greater 
+    // This event callback is triggered when user hold touch and move. In this callback we capture currentPointerCaptureposition and if the currentPointerCapturePosition is greater 
     // then touchStartPosition then its a right swipe else its a left swipe. Then based on the startTouchvalue and currentTouchValue we find how much we have to transition 
     // the slider. Then before transitioning the slider we check if the transition amount is > 0 or < container width we devide it by 6 to create animation like the slider 
     // cannot slide anymore. then we transition the slider and in touchend callback we potion the slider based on current transition 
-    const handleTouchMove = (e) => {
+    const handlePointerMove = (e) => {
         
         if (!maxRightTransition.current){
             maxRightTransition.current = -(((mainContainer.current.firstChild.clientWidth + 20) * reviewData.length) - 20 - mainContainer.current.parentNode.clientWidth)
         }
         // Get Touches object
+        // if event type is mousemove then check if the mouse button is pressed. It the button is pressed then only transition the slider else do nothing
         if (e.type === "mousemove"){
-            console.log(mousePressed)
             if (mousePressed){
-                console.log("AaSSSDD")
                 // get current Page Position same as clientX
-                currentTouchPosition = e.clientX
-                let tr = currentTransPosition1.current + (startTouchPosition > currentTouchPosition ? -(startTouchPosition - currentTouchPosition) : (currentTouchPosition - startTouchPosition))
+                currentPointerCapturePosition = e.clientX
+                let tr = currentTransPosition1.current + (startPointerCapturePosition > currentPointerCapturePosition ? -(startPointerCapturePosition - currentPointerCapturePosition) : (currentPointerCapturePosition - startPointerCapturePosition))
                 if (tr > 0){
                     tr /= 6    
                 }
@@ -150,8 +135,8 @@ const TestimonialSlider = () => {
 
             const touches = e.touches ;
             // get current Page Position same as clientX
-            currentTouchPosition = touches[0].clientX
-            let tr = currentTransPosition1.current + (startTouchPosition > currentTouchPosition ? -(startTouchPosition - currentTouchPosition) : (currentTouchPosition - startTouchPosition))
+            currentPointerCapturePosition = touches[0].clientX
+            let tr = currentTransPosition1.current + (startPointerCapturePosition > currentPointerCapturePosition ? -(startPointerCapturePosition - currentPointerCapturePosition) : (currentPointerCapturePosition - startPointerCapturePosition))
             if (tr > 0){
                 tr /= 6    
             }
@@ -166,14 +151,11 @@ const TestimonialSlider = () => {
             console.log(tr)
             currentTransPosition.current = tr
         }
-
-
-
     }
 
     return (
         <div className={style['outer-container']}>
-            <div className={style['inner-container'] + " inner-container"} onTouchMove={handleTouchMove} onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd} onMouseDown={handleTouchStart} onMouseUp={handleTouchEnd} onMouseMove={handleTouchMove} ref={mainContainer}>
+            <div className={style['inner-container'] + " inner-container"} onTouchMove={handlePointerMove} onTouchStart={handlePointerStart} onTouchEnd={handlePointerEnd} onMouseDown={handlePointerStart} onMouseUp={handlePointerEnd} onMouseMove={handlePointerMove} ref={mainContainer}>
                 {renderReviewData}
             </div>
             <style jsx>{`
