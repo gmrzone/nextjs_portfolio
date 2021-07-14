edb8128a06b13883ed83ebe3ced219594cabb501
AFzal <saiyedafzalgz@gmail.com>
2021-07-14 06:20:18 -0700

    added Dark Theme in about section

--- components/common/SectionHeader.js
+++ components/common/SectionHeader.js
@@ -1,8 +1,8 @@
 const SectionHeader = ({ title, meta, inverted = false }) => {
     return (
         <div className="mb-10 desktop-st:mb-16">
-            <h2 className={`text-center ${inverted ? "text-white" : "text-main"}`}>{title}</h2>
-            <span className={`text-center block font-semibold ${inverted ? "text-gray-400" : "text-gray-500"}`}>{meta}</span>
+            <h2 className={`text-center ${inverted ? "text-white" : "text-main dark:text-main-dark"}`}>{title}</h2>
+            <span className={`text-center block font-semibold ${inverted ? "text-gray-400" : "text-gray-500 dark:text-gray-700"}`}>{meta}</span>
         </div>
     );
 };
--- components/home/about/AboutContent.js
+++ components/home/about/AboutContent.js
@@ -5,15 +5,15 @@ const AboutContent = () => {
             className="w-full flex flex-col justify-center desktop-st:w-2/3 translate-x-full opacity-0 transition-all duration-500"
             data-name="content">
             <div className="space-y-8">
-                <h3 className="text-main">I am AFzal and I&apos;am a Fullstack Developer</h3>
-                <p className="text-gray-700">
+                <h3 className="text-main dark:text-main-dark">I am AFzal and I&apos;am a Fullstack Developer</h3>
+                <p className="text-gray-700 dark:text-sec-dark font-semibold">
                     Highly motivated self-thought developer seeking to launch a career building web application. High level experiance in
                     web design and development Knowledge producing quality work. Familiar with development and deployment process of many
                     web-based technologies such as Python, Django, DRF, JavaScript, JQuery, Node, React, Redux and Next.js etc..
                 </p>
             </div>
             <div className="mt-8">
-                <ButtonLink text="Download CV" icon="far fa-file text-xl" href="#" download />
+                <ButtonLink text="Download CV" icon="far fa-file text-xl" cssClasses="dark:bg-sec-dark dark:text-bg-sec-inverted dark:hover:bg-main-dark" href="#" download />
             </div>
         </div>
     );
