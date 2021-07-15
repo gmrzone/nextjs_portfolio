c49d4d9ea153e9ef7c0b44a4a884a47e834ace24
AFzal <saiyedafzalgz@gmail.com>
2021-07-15 12:25:48 -0700

    restyled dark nav

--- components/common/MainLayout.js
+++ components/common/MainLayout.js
@@ -3,31 +3,10 @@ import Footer from "../shared/footer/Footer";
 import { useEffect, useRef } from 'react'
 const MainLayout = ({ children }) => {
     const headerRef = useRef()
-    useEffect(() => {
-        const handleScroll = (e) => {
-            if(window.pageYOffset > 0){
-                headerRef.current.classList.remove('dark:bg-nav-dark')
-                headerRef.current.classList.remove('bg-bg-sec')
-                headerRef.current.classList.add('dark:bg-sec-inverted')
-                headerRef.current.classList.add('bg-white')
-                headerRef.current.classList.add('shadow-md')
-            }
-            else{
-                headerRef.current.classList.remove('bg-white')
-                headerRef.current.classList.remove('dark:bg-sec-inverted')
-                headerRef.current.classList.remove('shadow-md')
-                headerRef.current.classList.add('dark:bg-nav-dark')
-                headerRef.current.classList.add('bg-bg-sec')
-            }
-            
-        }
-        window.addEventListener('scroll', handleScroll)
 
-
-    }, [])
     return (
         <div className="main-body h-full flex flex-col">
-            <header id="header" className="bg-bg-sec transition-colors duration-500 dark:bg-nav-dark fixed w-full z-50" ref={headerRef}>
+            <header id="header" className="bg-bg-sec transition-colors duration-500 dark:bg-bg-sec-inverted sm:dark:bg-sec-dark fixed w-full z-50" ref={headerRef}>
                 <Navbar />
             </header>
             <main id="main" className="bg-bg-sec dark:bg-bg-sec-dark transition-colors duration-300">
